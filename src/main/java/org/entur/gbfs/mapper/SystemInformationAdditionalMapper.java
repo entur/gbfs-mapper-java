03b7e5a66a9d4017d74e104e77cc6dc715d00dcd
Tom Erik Støwer <tom.erik.stower@entur.org>
2024-05-27 12:56:55 +0200

    Migrate java model to mobilitydata package

--- src/main/java/org/entur/gbfs/mapper/SystemInformationAdditionalMapper.java
+++ src/main/java/org/entur/gbfs/mapper/SystemInformationAdditionalMapper.java
@@ -11,119 +11,119 @@ public abstract class SystemInformationAdditionalMapper {
 
     @SystemInformationDataMapper
     @Mapping(target = "languages", expression = "java(List.of(language))")
-    abstract org.entur.gbfs.v3_0.system_information.GBFSData mapData(org.entur.gbfs.v2_3.system_information.GBFSData source, @Context String language);
+    abstract org.mobilitydata.gbfs.v3_0.system_information.GBFSData mapData(org.mobilitydata.gbfs.v2_3.system_information.GBFSData source, @Context String language);
 
     @SystemInformationDataMapper
     @Mapping(target = "language", expression = "java(language)")
-    abstract org.entur.gbfs.v2_3.system_information.GBFSData mapDataInverse(org.entur.gbfs.v3_0.system_information.GBFSData source, @Context String language);
+    abstract org.mobilitydata.gbfs.v2_3.system_information.GBFSData mapDataInverse(org.mobilitydata.gbfs.v3_0.system_information.GBFSData source, @Context String language);
 
-    List<org.entur.gbfs.v3_0.system_information.GBFSName> mapName(String value, @Context String language) {
+    List<org.mobilitydata.gbfs.v3_0.system_information.GBFSName> mapName(String value, @Context String language) {
         if (value == null) {
             return null;
         }
         return List.of(
-                new org.entur.gbfs.v3_0.system_information.GBFSName()
+                new org.mobilitydata.gbfs.v3_0.system_information.GBFSName()
                         .withText(value)
                         .withLanguage(language)
         );
     }
 
-    String mapNameInverse(List<org.entur.gbfs.v3_0.system_information.GBFSName> values, @Context String language) {
+    String mapNameInverse(List<org.mobilitydata.gbfs.v3_0.system_information.GBFSName> values, @Context String language) {
         if (values == null) {
             return null;
         }
 
         return values.stream()
                 .filter(name -> name.getLanguage().equals(language))
-                .map(org.entur.gbfs.v3_0.system_information.GBFSName::getText)
+                .map(org.mobilitydata.gbfs.v3_0.system_information.GBFSName::getText)
                 .findFirst().orElse(null);
     }
 
-    List<org.entur.gbfs.v3_0.system_information.GBFSShortName> mapShortName(String value, @Context String language) {
+    List<org.mobilitydata.gbfs.v3_0.system_information.GBFSShortName> mapShortName(String value, @Context String language) {
         if (value == null) {
             return null;
         }
         return List.of(
-                new org.entur.gbfs.v3_0.system_information.GBFSShortName()
+                new org.mobilitydata.gbfs.v3_0.system_information.GBFSShortName()
                         .withText(value)
                         .withLanguage(language)
         );
     }
 
-    String mapShortNameInverse(List<org.entur.gbfs.v3_0.system_information.GBFSShortName> values, @Context String language) {
+    String mapShortNameInverse(List<org.mobilitydata.gbfs.v3_0.system_information.GBFSShortName> values, @Context String language) {
         if (values == null) {
             return null;
         }
 
         return values.stream()
                 .filter(name -> name.getLanguage().equals(language))
-                .map(org.entur.gbfs.v3_0.system_information.GBFSShortName::getText)
+                .map(org.mobilitydata.gbfs.v3_0.system_information.GBFSShortName::getText)
                 .findFirst().orElse(null);
     }
 
-    List<org.entur.gbfs.v3_0.system_information.GBFSOperator> mapOperator(String operator, @Context String language) {
+    List<org.mobilitydata.gbfs.v3_0.system_information.GBFSOperator> mapOperator(String operator, @Context String language) {
         if (operator == null) {
             return null;
         }
         return List.of(
-                new org.entur.gbfs.v3_0.system_information.GBFSOperator()
+                new org.mobilitydata.gbfs.v3_0.system_information.GBFSOperator()
                         .withText(operator)
                         .withLanguage(language)
         );
     }
 
-    String mapOperatorInverse(List<org.entur.gbfs.v3_0.system_information.GBFSOperator> values, @Context String language) {
+    String mapOperatorInverse(List<org.mobilitydata.gbfs.v3_0.system_information.GBFSOperator> values, @Context String language) {
         if (values == null) {
             return null;
         }
 
         return values.stream()
                 .filter(name -> name.getLanguage().equals(language))
-                .map(org.entur.gbfs.v3_0.system_information.GBFSOperator::getText)
+                .map(org.mobilitydata.gbfs.v3_0.system_information.GBFSOperator::getText)
                 .findFirst().orElse(null);
     }
 
-    List<org.entur.gbfs.v3_0.system_information.GBFSTermsUrl> mapTermsUrl(String termsUrl, @Context String language) {
+    List<org.mobilitydata.gbfs.v3_0.system_information.GBFSTermsUrl> mapTermsUrl(String termsUrl, @Context String language) {
         if (termsUrl == null) {
             return null;
         }
         return List.of(
-                new org.entur.gbfs.v3_0.system_information.GBFSTermsUrl()
+                new org.mobilitydata.gbfs.v3_0.system_information.GBFSTermsUrl()
                         .withText(termsUrl)
                         .withLanguage(language)
         );
     }
 
-    String mapTermsUrlInverse(List<org.entur.gbfs.v3_0.system_information.GBFSTermsUrl> values, @Context String language) {
+    String mapTermsUrlInverse(List<org.mobilitydata.gbfs.v3_0.system_information.GBFSTermsUrl> values, @Context String language) {
         if (values == null) {
             return null;
         }
 
         return values.stream()
                 .filter(name -> name.getLanguage().equals(language))
-                .map(org.entur.gbfs.v3_0.system_information.GBFSTermsUrl::getText)
+                .map(org.mobilitydata.gbfs.v3_0.system_information.GBFSTermsUrl::getText)
                 .findFirst().orElse(null);
     }
 
-    List<org.entur.gbfs.v3_0.system_information.GBFSPrivacyUrl> mapPrivacyUrl(String privacyUrl, @Context String language) {
+    List<org.mobilitydata.gbfs.v3_0.system_information.GBFSPrivacyUrl> mapPrivacyUrl(String privacyUrl, @Context String language) {
         if (privacyUrl == null) {
             return null;
         }
         return List.of(
-                new org.entur.gbfs.v3_0.system_information.GBFSPrivacyUrl()
+                new org.mobilitydata.gbfs.v3_0.system_information.GBFSPrivacyUrl()
                         .withText(privacyUrl)
                         .withLanguage(language)
         );
     }
 
-    String mapPrivacyUrlInverse(List<org.entur.gbfs.v3_0.system_information.GBFSPrivacyUrl> values, @Context String language) {
+    String mapPrivacyUrlInverse(List<org.mobilitydata.gbfs.v3_0.system_information.GBFSPrivacyUrl> values, @Context String language) {
         if (values == null) {
             return null;
         }
 
         return values.stream()
                 .filter(name -> name.getLanguage().equals(language))
-                .map(org.entur.gbfs.v3_0.system_information.GBFSPrivacyUrl::getText)
+                .map(org.mobilitydata.gbfs.v3_0.system_information.GBFSPrivacyUrl::getText)
                 .findFirst().orElse(null);
     }
 }
--- src/test/java/org/entur/gbfs/mapper/SystemAlertsMapperTest.java
+++ src/test/java/org/entur/gbfs/mapper/SystemAlertsMapperTest.java
@@ -21,8 +21,8 @@ class SystemAlertsMapperTest {
     @Test
     void testMapSystemAlertsFile() throws IOException {
         URL resource = getClass().getClassLoader().getResource("fixtures/v2_3/system_alerts.json");
-        org.entur.gbfs.v2_3.system_alerts.GBFSSystemAlerts testSubject = objectMapper.readValue(resource, org.entur.gbfs.v2_3.system_alerts.GBFSSystemAlerts.class);
-        org.entur.gbfs.v3_0.system_alerts.GBFSSystemAlerts mapped = GBFSMapper.INSTANCE.map(testSubject, "en");
+        org.mobilitydata.gbfs.v2_3.system_alerts.GBFSSystemAlerts testSubject = objectMapper.readValue(resource, org.mobilitydata.gbfs.v2_3.system_alerts.GBFSSystemAlerts.class);
+        org.mobilitydata.gbfs.v3_0.system_alerts.GBFSSystemAlerts mapped = GBFSMapper.INSTANCE.map(testSubject, "en");
         assertDoesNotThrow(() -> {
             expect
                     .serializer("json")
@@ -34,8 +34,8 @@ class SystemAlertsMapperTest {
     @Test
     void testMapSystemAlertsFileInverse() throws IOException {
         URL resource = getClass().getClassLoader().getResource("fixtures/v3_0/system_alerts.json");
-        org.entur.gbfs.v3_0.system_alerts.GBFSSystemAlerts testSubject = objectMapper.readValue(resource, org.entur.gbfs.v3_0.system_alerts.GBFSSystemAlerts.class);
-        org.entur.gbfs.v2_3.system_alerts.GBFSSystemAlerts mapped = GBFSMapper.INSTANCE.map(testSubject, "en");
+        org.mobilitydata.gbfs.v3_0.system_alerts.GBFSSystemAlerts testSubject = objectMapper.readValue(resource, org.mobilitydata.gbfs.v3_0.system_alerts.GBFSSystemAlerts.class);
+        org.mobilitydata.gbfs.v2_3.system_alerts.GBFSSystemAlerts mapped = GBFSMapper.INSTANCE.map(testSubject, "en");
         assertDoesNotThrow(() -> {
             expect
                     .serializer("json")
